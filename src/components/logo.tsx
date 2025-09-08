05be87a753e21a63edd495c12d0fed10cc4c531a
Douglas Vini <douglasdannuz@gmail.com>
2025-09-08 19:09:32 +0000

    a logo do site deve aparecer aqui  e aqui

--- src/components/logo.tsx
+++ src/components/logo.tsx
@@ -1,13 +1,10 @@
-import { BrainCircuit } from 'lucide-react';
+import Image from 'next/image';
 import Link from 'next/link';
 
 export function Logo() {
   return (
     <Link href="/" className="flex items-center gap-2" aria-label="DelviFy Home">
-      <BrainCircuit className="h-7 w-7 text-primary" />
-      <span className="text-xl font-bold font-headline tracking-tighter text-foreground">
-        DelviFy
-      </span>
+      <Image src="https://picsum.photos/128/32" width={100} height={32} alt="DelviFy Logo" data-ai-hint="logo company" />
     </Link>
   );
 }
