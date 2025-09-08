fc08daf8d45114818944b02a18d0582a22479f72
Douglas Vini <douglasdannuz@gmail.com>
2025-09-08 15:21:16 +0000

    Try fixing this error: `Console Error: Error: Service account object mus

--- src/app/admin/profile/actions.ts
+++ src/app/admin/profile/actions.ts
@@ -9,9 +9,9 @@ import { credential } from 'firebase-admin';
 // Initialize Firebase Admin SDK
 const firebaseConfig = {
   credential: credential.cert({
-    projectId: process.env.FIREBASE_PROJECT_ID,
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
-    privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    private_key: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, '\n'),
   }),
   storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
 };
