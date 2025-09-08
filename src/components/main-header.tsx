2d0012f1d0f190b15d98be9f9a2dfc861f4fc645
Douglas Vini <douglasdannuz@gmail.com>
2025-09-08 18:35:10 +0000

    nafa melhorou ainfdana nav ar  e footer  eles nçao obecrem

--- src/components/main-header.tsx
+++ src/components/main-header.tsx
@@ -12,7 +12,7 @@ export function MainHeader() {
 
   return (
     <header className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
-      <div className="container flex h-16 items-center px-4 md:px-6">
+      <div className="container flex h-16 items-center">
         <Logo />
         <nav className="ml-10 hidden gap-6 md:flex">
           {navItems.map((item) => (
