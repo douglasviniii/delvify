a3033b3e291838471cb505335cb4a791dede1b4a
Douglas Vini <douglasdannuz@gmail.com>
2025-09-08 16:55:33 +0000

    COLOCAR ESSAS CAHVECS DIRETO NO CODGO MEMSO {   "type": "service_account

--- src/lib/firebase-admin.ts
+++ src/lib/firebase-admin.ts
@@ -1,27 +1,26 @@
 
-import 'dotenv/config';
 import { initializeApp, getApps, App, cert, ServiceAccount } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 import { getStorage } from 'firebase-admin/storage';
 
 let adminApp: App;
 
+const serviceAccount: ServiceAccount = {
+  "projectId": "venda-fcil-pdv",
+  "privateKey": "-----BEGIN PRIVATE KEY-----\nMIIEvQIBADANBgkqhkiG9w0BAQEFAASCBKcwggSjAgEAAoIBAQC3leDeVF7eOtbH\n9RAltEF0J90wgqhW1AG/w+E24ylGewS5oecUrR9VJ2FQTMHbn7Ig/QunrZ6ulXtx\nGcHZYMxmjvBfn+2qXqHq8VQmmFgyO0TV4UlB1KjWB1Y57s5t27sTcbY/21xtIe8L\n1dRX+Eo3U+HcFnAz500g1u+F7SZwRBmGvjq0Sopdv65F0/SAu/oxAr29nG5p3P4M\nloxT3wkdPnRw3dsBKWnHCOdMMIWW4YhLdSOMjbEKRd1A19i1xBOKyAKa4AMnceII\nE9q7ohtwr2allsWj0rWaKammFkm9OD2cdN5uZaTpvsgWG4fSz2aK2S4UcPv5O7DM\nIzAf/M+pAgMBAAECggEADjkWA7eO5YHyualga8FiOErt2agnvVldvNMCx+czv46A\nlFapIdqTRLxBL3DQ7JBb4y8jl+UtN+qkFoQ3tt0HXPM6R3hLGBHOktaJTder7pFV\nsQnMDIX6oiFNon2gP9XgEZ6zz1DyAVD5lYXGlp2pd02gABZ7cSP8f1yzkCFKQvR7\n+X4GrYh3tiXSdcUeqt/aGRLQhFY0qeaN5iy3sA2zScac83G61wGXhNiaPnjzk8xw\nMla1u2+rdIdsvG6UzQ4z6Pf+O8qnJxgYkq3hk0nnU1BJggGughNV4aKJ9KIM3uwC\nVaMHrXfpYPMgkMWWTZO+VBwdNMm6rIaY27NeynJVAQKBgQDtEOTmYLP8u5B31VIW\nkqjXEfxXB+vFSTF9sWGYHN5nE6RuPnnUdWOQplFmIuoH1PbbAjvpoRiQVNBiby80\nx7wVw61Q/LU56s46FL9ZdQt+3hwtyPUgpBp2Cl0mh5TrIGBn4+e7V5WVfGzj0xd/\nTmNrn9F97CZJC/PL/eQ0T7eUqQKBgQDGP4JPFg/23Cv3voJJOTfWfkQaqBDF9lzx\n5XVPIhpS0REy4n9wiQp/62KwCqES+YkDmnEeiNKRPfOECliX8r7KNWcBq4i6KVj1\nAnB+eAhCHwUaqIAdWboUA11u0qYGSPNgVht2ZS3v29CB6rXg/hUS6AhvTbm0zGf1\n1R+EDpVDAQKBgQCpuvDH6PC0wG28/mRZeQOdiGkMvsUVaUQf5AIl8HVjg3K049JC\nRRHWHN4mrFS26skbIMxYh1iY7cCM2WII/gAx7PmIBIaUQwMIHpapq91hJhEyzrCC\ngDvZy63JykTa20Fq4IenYBve/UjRDO/D3BHemnxZFdyLbB1PLiZXNcQQkQKBgG6e\nET+/t7iusXnTOy9QVe/BFI8rJ/DNvp7awdId3UJIlagm6aUJUmp+FNrVk3ra8bCp\nBGVdQuD4CGCsxTJDqGF72rX72JbHa3OKoOpwX2tFk7uEObgm0MVJ+2BS+YCYQ/SF\nF13ApxknNfjH1iRsoaWjAtHYNL7FL1zkRmmRGYgBAoGACAS0/2bINoTlgCXXF+Sf\nzvqHkiKe+M1y+MILtBPFvnCKZckATf1vvdB6i2Fp2ipNoBZFKaWENgBT5DYw1x9O\nHv61zjxGdsQM1pXlG9aTotbYkt596VFMExUddUXdppKSMXsi+Azs6lFFj8ZGXLZz\nQkDOPStxn2F/LBZkbjX74gs=\n-----END PRIVATE KEY-----\n".replace(/\\n/g, '\n'),
+  "clientEmail": "firebase-adminsdk-fbsvc@venda-fcil-pdv.iam.gserviceaccount.com",
+};
+
+
 if (!getApps().length) {
   try {
-    const serviceAccountString = process.env.FIREBASE_SERVICE_ACCOUNT_JSON;
-    if (!serviceAccountString) {
-        throw new Error('A variável de ambiente FIREBASE_SERVICE_ACCOUNT_JSON não está definida.');
-    }
-
-    const serviceAccount: ServiceAccount = JSON.parse(serviceAccountString);
-
     adminApp = initializeApp({
       credential: cert(serviceAccount),
       storageBucket: 'venda-fcil-pdv.firebasestorage.app',
     });
   } catch (error: any) {
     console.error('Erro detalhado na inicialização do Firebase Admin:', error);
-    throw new Error('Falha na inicialização do Firebase Admin. Verifique a variável de ambiente FIREBASE_SERVICE_ACCOUNT_JSON. Erro original: ' + error.message);
+    throw new Error('Falha na inicialização do Firebase Admin. Verifique as credenciais. Erro original: ' + error.message);
   }
 } else {
   adminApp = getApps()[0];
