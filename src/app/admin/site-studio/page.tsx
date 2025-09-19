3747705dce12689432e2d4fe7f6ca2dff5eb21d2
Douglas Vini <douglasdannuz@gmail.com>
2025-09-19 14:09:29 +0000

    NÃO CONSIGO EDITAR NADA OPIS AS PAGINAS DE EDIÇÃO FICA ASIS, 404 This pa

--- src/app/admin/site-studio/page.tsx
+++ src/app/admin/site-studio/page.tsx
@@ -95,7 +95,7 @@ export default function AdminSiteStudioPage() {
 
 
   return (
-    <div className="space-y-6">
+    <div className="h-full w-full space-y-6">
       <div className="flex items-center justify-between">
         <div>
             <h1 className="font-headline text-3xl font-bold tracking-tight">Estúdio de Site</h1>
