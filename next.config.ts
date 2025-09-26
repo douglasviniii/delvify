ccc3c62cb3b71b45da75b12a4a3f7ce330ff6047
Douglas Vini <douglasdannuz@gmail.com>
2025-09-26 13:05:51 +0000

    tem como rever isos pra fazer o deploy na render?

--- next.config.ts
+++ next.config.ts
@@ -67,15 +67,6 @@ const nextConfig: NextConfig = {
       }
     ],
   },
-  webpack: (config, { isServer }) => {
-    // Exclui o favicon.ico de ser processado pelos loaders de módulo
-    config.module.rules.push({
-      test: /favicon\.ico$/,
-      loader: 'ignore-loader',
-    });
-
-    return config;
-  },
 };
 
 export default nextConfig;
